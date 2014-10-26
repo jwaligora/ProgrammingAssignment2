03acb7789bea08d20e6c69757b7f3d878136512b
Joanna Waligora <joanna.waligora@gmail.com>
2014-10-26 02:48:44 -0400

    Defined makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# To save on CPU cycles, 
+# the makeCacheMatrix and cacheSolve functions
+# provide a caching utility for storing and retrieving
+# a matirx along with its inverse. 
+#
+# 
+#
+# The makeCacheMatrix function creates a list object with functions to:
+#  > set the value of the matrix
+#  > get the value of the matrix
+#  > set the value of the matrix inverse
+#  > get the value of the matrix inverse
+# This function offers no error handling; it assumes user will provide
+# a square, non-singular matrix, and a valid inverse 
+# (if not set via cacheSolve)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+# The cacheSolve function computes, caches, and returns
+# the inverse of a matrix cached by the makeCacheMatrix function. 
+# If the inverse has already been calculated 
+# (and the matrix has not changed), 
+# cacheSolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+# # Usage Example:
+
+# # Create a 3x3 matrix from random, normal data
+# dat <- matrix(rnorm(9,80,5), 3, 3, T)
+
+# # initiate matrix container
+# mat <- makeCacheMatrix()
+
+# # store matrix in container
+# mat$set(dat)
+
+# # validate it worked by retrieving stored matrix
+# mat$get()
+
+# # generate inverse
+# cacheSolve(mat)
+
+# # check for identiy matrix
+# mat$get() %*% mat$getinverse() 
